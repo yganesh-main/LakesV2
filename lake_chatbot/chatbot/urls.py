87ad44218926c38ab4a05c13249ef2f334eca8d1
Darshan-vitg <darshan.vaghasiya@vitg.us>
2025-04-15 22:30:56 -0400

    Delete lake_chatbot directory

--- lake_chatbot/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('handle_prompt/', views.handle_prompt, name='handle_prompt'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
