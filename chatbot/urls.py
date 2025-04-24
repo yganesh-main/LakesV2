2d8e57624ef185ded31a0d13ad3d1d046fbc4180
Darshan-vitg <darshan.vaghasiya@vitg.us>
2025-04-24 13:59:43 -0400

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('handle_prompt/', views.handle_prompt, name='handle_prompt'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('debug_session/', views.debug_session, name='debug_session'),
+    path('clear_prompt_history/', views.clear_prompt_history, name='clear_prompt_history'),
+]
+
